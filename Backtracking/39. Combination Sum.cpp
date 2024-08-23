8cfe20e817ffd5c39ccaf3ec2c7507bb4d025d16
vikash sinha <103680992+vikash000x@users.noreply.github.com>
2024-08-23 14:37:13 +0530

    Update 39. Combination Sum.cpp

--- Backtracking/39. Combination Sum.cpp
+++ Backtracking/39. Combination Sum.cpp
@@ -47,3 +47,25 @@ void combination(vector<int>& candidates, int target, vector<int> curr, vector<v
 
    
 };
+
+
+class Solution {
+public:
+  void combination(vector<int> &candidates, int target, vector<vector<int>>& result, vector<int>curr, int idx){
+    if(target<0) return;
+    if(target == 0) result.push_back(curr);
+
+    for(int i=idx; i<candidates.size(); i++){
+        curr.push_back(candidates[i]);
+        combination(candidates, target-candidates[i], result, curr, i);
+        curr.pop_back();
+    }
+  }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> result;
+        vector<int> curr;
+        combination(candidates, target, result, curr, 0);
+        return result;
+        
+    }
+};
